e223bde8203e4aeceed31806e66c0be29ead10fa
duwlsssss <rladuwls0814@gmail.com>
2024-12-14 16:22:41 +0900

    feat(programmers): [Lv2] 모음사전 풀이 추가

--- 김여진/week4/[Lv2] 모음사전.js
+++ 김여진/week4/[Lv2] 모음사전.js
@@ -1,3 +1,4 @@
+// 풀이 1
 // 문제 접근
 // 1. 모든 가능한 단어를 DFS로 미리 탐색하여 사전을 구성
 // 2. 주어진 word의 순서를 찾아 반환
@@ -21,4 +22,13 @@ const dfs = (now) => {
 function solution(word) {
     dfs('');
     return answer[word];
+}
+
+// 풀이 2
+// 문제 접근
+// 1. 각 자리가 정해지면 뒤에 올 수 있는 글자의 경우의 수가 정해진다.
+// 2. 각 자리마다 다음에 올 수 있는 경우의 수를 더하면 word의 순서를 알 수 있다.
+// 시간복잡도: O(1) 
+function solution(word) {
+  return [...word].reduce((acc, c, i) => acc + 'AEIOU'.indexOf(c) * Math.floor(781/5**i) + 1, 0);
 }
