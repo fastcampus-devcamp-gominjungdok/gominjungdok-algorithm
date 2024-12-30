b5d1212a5fbd0416357be6ff823d8c7a7ea7ff71
duwlsssss <rladuwls0814@gmail.com>
2024-12-30 10:33:57 +0900

    feat(baekjoon): [Silver2] 잃어버린 괄호 문제 조건 반영

--- 김여진/week6/[Silver2] 잃어버린 괄호.js
+++ 김여진/week6/[Silver2] 잃어버린 괄호.js
@@ -1,7 +1,7 @@
 // 문제 접근
 // 1. `-` 연산자 기준으로 식을 그룹화 
-// 2. 그룹화한 배열을 순회하며 `''`, 즉 `-` 앞에 수가 없는 경우는 0으로 바꿈, 이 외 그룹은 `+`로 나눠 합 계산
-// 3. 첫번째 요소를 제외한 나머지 요소들 합 계산해 빼서 반환 
+// 2. 그룹화한 배열을 순회하며 `+`로 나눠 합 계산
+// 3. 첫번째 요소를 제외한 나머지 요소들 합 빼서 반환 
 // 시간복잡도 O(n)
 
 const fs = require('fs')
@@ -11,9 +11,6 @@ const input = fs.readFileSync(filePath).toString().trim()
 const groups = input.split('-');
 
 const groupSums = groups.map(group => {
-    if(group === ''){
-      return 0
-    }
     return group
       .split('+')
       .reduce((sum, n) => sum + Number(n), 0)
